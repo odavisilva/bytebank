e7788729e363c0a0c03c2f68239b511bdd0e8733
odavisilva <daviolie2@gmail.com>
2025-02-13 16:43:02 -0300

    Basic for array

--- bytebank_ATENDIMENTO/Program.cs
+++ bytebank_ATENDIMENTO/Program.cs
@@ -1,4 +1,25 @@
-﻿using bytebank_ATENDIMENTO.bytebank.Atendimento;
-Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
-new ByteBankAtendimento().AtendimentoCliente();
+﻿Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
 
+TestaArrrayInt();
+void TestaArrrayInt ()
+{ 
+    int[] idades = new int[5];
+    idades[0] = 30;
+    idades[1] = 40;
+    idades[2] = 17;
+    idades[3] = 21;
+    idades[4] = 18;
+
+    Console.WriteLine($"Tamanho do Array {idades.Length}");
+
+    int acumulador = 0;
+
+    for (int i = 0; i< idades.Length; i++)
+    {
+        int idade = idades[i];
+        Console.WriteLine($"Índice [{i}] = {idade}");
+        acumulador += idade;
+    }
+    int media = acumulador / idades.Length;
+    Console.WriteLine($"Média de idades = {media}");
+} 
